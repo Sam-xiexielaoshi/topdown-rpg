4a40340bb55a27392769923ccedcaa586a6b95d9
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2026-03-05 20:52:32 +0530

    clampped the map and increased the camera movement speed

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <raylib.h>
+#include<raymath.h>
 
 int main(){
 
@@ -15,14 +16,28 @@ int main(){
 
     //map variable
     Texture2D map = LoadTexture("nature tileset/map.png");
-    
+    Vector2 bgPos{0.0,0.0};
+    float Speed{4.0};
 
     while(!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
+        Vector2 directions{};
+        if(IsKeyDown(KEY_A)) directions.x-=1.0;
+        if(IsKeyDown(KEY_D)) directions.x+=1.0;
+        if(IsKeyDown(KEY_W)) directions.y-=1.0;
+        if(IsKeyDown(KEY_S)) directions.y+=1.0;
+        if(Vector2Length(directions)!=0.0){
+            //set bgPos = bgPos - directions
+            
+            bgPos=Vector2Subtract(bgPos, Vector2Scale(Vector2Normalize(directions), Speed));
+        }
+
+        bgPos.x = Clamp(bgPos.x, -(map.width * 4.0f - windowDimensions[0]), 0.0f);
+        bgPos.y = Clamp(bgPos.y, -(map.height * 4.0f - windowDimensions[1]), 0.0f);
+
         //draw the background
-        Vector2 bgPos{0.0,0.0};
         DrawTextureEx(map, bgPos, 0.0,4.0,WHITE);
 
         EndDrawing();
