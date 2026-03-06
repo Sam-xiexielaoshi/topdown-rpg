ade271324de3efc934c0240d307722ecc6881e49
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2026-03-07 02:49:20 +0530

    World position vs screen position || Render() function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <raylib.h>
 #include <raymath.h>
 #include "Character.h"
+#include "Prop.h"
 
 int main()
 {
@@ -14,11 +15,13 @@ int main()
 
     Character knight{windowWidth, windowHeight};
 
+    Prop rock{Vector2{0.f, 0.f}, LoadTexture("nature_tileset/Rock.png")};
+
     // set target FPS
     SetTargetFPS(60);
 
     // map variable
-    Texture2D map = LoadTexture("nature tileset/map.png");
+    Texture2D map = LoadTexture("nature_tileset/map.png");
     Vector2 bgPos{0.0f, 0.0f};
     const float mapScale{4.0f};
 
@@ -29,8 +32,12 @@ int main()
 
         bgPos = Vector2Scale(knight.getWorldPos(), -1.f);
 
+        
         // draw the background
         DrawTextureEx(map, bgPos, 0.0, mapScale, WHITE);
+
+        rock.Render(knight.getWorldPos());
+        
         knight.tick(GetFrameTime());
         // check map bounds
         if (knight.getWorldPos().x < 0.f || knight.getWorldPos().y < 0.f || knight.getWorldPos().x + windowWidth > map.width * mapScale || knight.getWorldPos().y + windowHeight > map.height * mapScale)
