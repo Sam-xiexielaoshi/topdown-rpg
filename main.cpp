2a513fa7c6cebc23ab33494e5348d123f3f018c6
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2026-03-07 01:57:48 +0530

    checking map bounds

--- main.cpp
+++ main.cpp
@@ -21,20 +21,26 @@ int main()
     // map variable
     Texture2D map = LoadTexture("nature tileset/map.png");
     Vector2 bgPos{0.0f, 0.0f};
+    const float mapScale{4.0f};
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        bgPos.x = Clamp(bgPos.x, -(map.width * 4.0f - windowWidth), 0.0f);
-        bgPos.y = Clamp(bgPos.y, -(map.height * 4.0f - windowHeight), 0.0f);
+        // bgPos.x = Clamp(bgPos.x, -(map.width * mapScale - windowWidth), 0.0f);
+        // bgPos.y = Clamp(bgPos.y, -(map.height * mapScale - windowHeight), 0.0f);
 
         bgPos = Vector2Scale(knight.getWorldPos(), -1.f);
 
         // draw the background
-        DrawTextureEx(map, bgPos, 0.0, 4.0, WHITE);
+        DrawTextureEx(map, bgPos, 0.0, mapScale, WHITE);
         knight.tick(GetFrameTime());
+        //check map bounds
+        if(knight.getWorldPos().x<0.f||knight.getWorldPos().y<0.f||knight.getWorldPos().x+windowWidth>map.width*mapScale||knight.getWorldPos().y+windowHeight>map.height*mapScale)
+        {
+            DrawText("Out of bounds!", 10, 10, 20, RED);
+        }
 
         EndDrawing();
     }
