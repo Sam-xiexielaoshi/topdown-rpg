4cbc6f6c654ea569e7fd4969d58e00bf006e3316
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2026-03-07 04:34:13 +0530

    created basic enemy ai

--- Enemy.cpp
+++ Enemy.cpp
@@ -9,10 +9,20 @@ Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture)
     run = run_texture;
     width = texture.width / maxframe;
     height = texture.height;
+    Speed = 3.5f;
 }
 
 void Enemy::tick(float deltaTime)
 {
+    // get toTarget
+    Vector2 toTarget = Vector2Subtract(target->getScreenPos(), screenPos);
+    // nomalize toTarget
+    toTarget = Vector2Normalize(toTarget);
+    // miltiply toTarget with speed
+    toTarget = Vector2Scale(toTarget, Speed);
+    // move the enemy
+    worldPos = Vector2Add(worldPos, toTarget);
+
     screenPos = Vector2Subtract(worldPos, target->getWorldPos());
     BaseCharacter::tick(deltaTime);
 }
