95fa38c916080d4847cd02635ed851eff315c30e
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2026-03-07 04:01:55 +0530

    bug fixing

--- Prop.cpp
+++ Prop.cpp
@@ -1,16 +1,18 @@
 #include "Prop.h"
 #include "raymath.h"
 
-Prop::Prop(Vector2 pos, Texture2D tex) : worldPos(pos), texture(tex){
-
+Prop::Prop(Vector2 pos, Texture2D tex) : texture(tex), worldPos(pos)
+{
 }
 
-void Prop::Render(Vector2 knightPos){
+void Prop::Render(Vector2 knightPos)
+{
     Vector2 screenPos = Vector2Subtract(worldPos, knightPos);
-    DrawTextureEx(texture, screenPos, 0.f, scale,WHITE);
+    DrawTextureEx(texture, screenPos, 0.f, scale, WHITE);
 }
 
-Rectangle Prop:: getCollisionRec(Vector2 knightPos){
+Rectangle Prop::getCollisionRec(Vector2 knightPos)
+{
     Vector2 screenPos{Vector2Subtract(worldPos, knightPos)};
     return Rectangle{
         screenPos.x,
